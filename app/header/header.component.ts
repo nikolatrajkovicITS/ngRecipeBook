ef82704d751996dd5ae9c60b6ebc8a4e2d9dd4f7
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-07-20 16:59:33 +0200

    Creating the components

---
+++ app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  title = 'HeaderComponent';
+}
+/**
+ * Created by nikola.trajkovic on 20/07/2017.
+ */
---
+++ app/recipes/recipes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent {
+  title = 'RecipesComponent';
+}
+/**
+ * Created by nikola.trajkovic on 20/07/2017.
+ */
