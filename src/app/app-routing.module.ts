d3136de3a65b95b4e5c525e7ae06d52a268da1d2
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-08-21 15:24:40 +0200

    Setting up Routes and Adding navigation to the App

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RecipesComponent } from './recipes/recipes.component';
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: 'recipes', component: RecipesComponent },
+    { path: 'shopping-list', component: ShoppingListComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { 
+    
+  }
