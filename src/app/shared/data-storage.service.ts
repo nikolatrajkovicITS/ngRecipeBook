c908aa22432c956736f8cc3f77e3994c003ff6cd
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-09-27 09:56:46 +0200

    Add get recipes from firebase method and Transforming Response Data to Prevent Errors

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
@@ -8,5 +9,26 @@ export class DataStorageService {
 
   storeRecipes() {
     return this.http.put('https://ng-recipe-book-2821a.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    return this.http.get('https://ng-recipe-book-2821a.firebaseio.com/recipes.json')
+      .map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              console.log(recipe);
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 }
