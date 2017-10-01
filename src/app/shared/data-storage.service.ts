c17bfc5a3937091352b27873b9f16e3cdb14646e
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-10-01 18:18:57 +0200

    Add Reducer

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,4 +1,4 @@
-import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -6,30 +6,33 @@ import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, 
+  constructor(private httpClient: HttpClient, 
               private recipeService: RecipeService,
               private authService: AuthService) { } 
 
   storeRecipes() {
     const token = this.authService.getToken();
     
-    return this.http.put('https://ng-recipe-book-2821a.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+    return this.httpClient.put('https://ng-recipe-book-2821a.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
     const token = this.authService.getToken();
 
-    return this.http.get('https://ng-recipe-book-2821a.firebaseio.com/recipes.json?auth=' + token)
+    return this.httpClient.get<Recipe[]>('https://ng-recipe-book-2821a.firebaseio.com/recipes.json?auth=' + token, {
+      observe: 'body',
+      responseType: 'json' 
+    })
       .map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
+        (recipes) => {
+          console.log(recipes);
           for (let recipe of recipes) {
             if (!recipe['ingredients']) {
               console.log(recipe);
               recipe['ingredients'] = [];
             }
           }
-          return recipes;
+          return [];
         }
       )
       .subscribe(
